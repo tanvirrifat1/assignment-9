9d4b59595996c212b9d3c795c06aa97352432881
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-10-11 20:50:36 +0600

    totify added

--- src/components/option/Option.js
+++ src/components/option/Option.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Optoin.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Option = ({ option }) => {
+    const notify = () => toast("Wow so easy!");
     return (
         <div>
-            <button className='btn mt-5 bg-blue-500'>{option}</button>
+            <button onClick={notify} className='btn mt-5 bg-blue-500'>{option}</button>
+            <ToastContainer />
         </div>
     );
 };
